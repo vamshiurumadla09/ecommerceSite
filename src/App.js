23ac3b4cf660ed5340cb1892dde6aa8e432e52e8
vamshiurumadla09 <urumadlavamshi@gmail.com>
2023-06-20 18:12:47 +0530

    ecommerce first push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import store from './store/store';
 import { Provider } from 'react-redux';
-import ReduxPractise from './components/ReduxPractise';
+import ProductApi from './components/ProductApi';
+import CountriesApi from './components/CountriesApi';
 function App() {
   return (
     <Provider store={store}>
-      <ReduxPractise></ReduxPractise>
+      <ProductApi></ProductApi>
+      <CountriesApi></CountriesApi>
     </Provider>
   )
 }
--- src/store/reduces/combinerReducers.js
+++ src/store/reduces/combinerReducers.js
@@ -1,7 +1,6 @@
 import { combineReducers } from "redux";
-import demoReducer from "./demoReducer";
 import counteriesApiReducer from "./countriesApiReducer";
 import productApiReducer from "./productsApiReducer";
 
-const reducer = combineReducers({demo:demoReducer, countriesApi:counteriesApiReducer, products:productApiReducer});
+const reducer = combineReducers({countriesApi:counteriesApiReducer, products:productApiReducer});
 export default reducer;
