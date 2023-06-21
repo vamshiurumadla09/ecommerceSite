a12c83e829f72e52f7c7ddf12349b6a2c71bcdc6
vamshiurumadla09 <urumadlavamshi@gmail.com>
2023-06-21 17:36:43 +0530

    ecommerce site

---
+++ src/components/DataFetchApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function DataFetchApp() {
+  return (
+    <div className='betterview'>DataFetchApp
+    <div>
+        <input type="text" />
+    </div>
+    </div>
+  )
+}
+
+export default DataFetchApp
